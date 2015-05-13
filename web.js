788b6b7a46104333d576e9aea347f4815290874a
Harry Cummings <harry.cummings@softwire.com>
2015-05-13 23:14:02 +0100

    Send reply

--- web.js
+++ web.js
@@ -1,10 +1,22 @@
 var express = require('express');
+var request = require('request');
 
 var app = express();
 
-app.all('/nexmo', function(req, res) {
+app.get('/nexmo', function(req, res) {
 	console.log({ method: req.method, query: req.query, body: req.body });
-	res.send(200, 'Message received');	
+	res.send(200, 'Message received');
+	request.post({
+		uri: 'https://rest.nexmo.com/sms/json',
+		json: true,
+		body: {
+			api_key: process.env.API_KEY,
+			api_secret: process.env.API_SECRET,
+			from: req.query.to,
+			to: req.query.msisdn,
+			text: 'Hello there'
+		}		
+	})
 });
 
 var port = process.env.PORT || 5000;
